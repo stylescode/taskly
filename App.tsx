28b613dacaefb1714a777b1b7129bf8384db42e7
Styles <stylesk11@gmail.com>
2024-08-20 15:25:33 -0700

    import list item component

--- App.tsx
+++ App.tsx
@@ -1,35 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View, TouchableOpacity, Pressable} from 'react-native';
-import { theme } from './theme';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+import { ListItem } from "./components/ListItem";
 
 export default function App() {
-
-  const handleDelete = () => {
-    Alert.alert("Are you sure you want to delete this item?", "", [
-      {
-        text: "Cancel",
-        style: "cancel",
-      },
-      {
-        text: "Delete",
-        style: "destructive",
-        onPress: () => console.log("Item deleted"),
-      },
-    ]);
-  }
-
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>COFFEE</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleDelete}
-          activeOpacity={0.9}
-        >
-        <Text style={styles.buttonText}>DELETE</Text>
-      </TouchableOpacity>
-      </View>
+      <ListItem itemName="TEA" isCompleted={false} />
+      <ListItem itemName="SUGAR" isCompleted={true} />
+      <ListItem itemName="CHOCOLATE" isCompleted={false} />
       <StatusBar style="auto" />
     </View>
   );
@@ -38,32 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-  itemContainer: {
-    alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: "#1a759f",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-  },
-  itemText: {
-    fontSize: 20,
-    fontWeight: "200",
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    borderRadius: 8,
-    paddingHorizontal: 20,
-    paddingVertical: 16,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    fontSize: 14,
-    fontWeight: "700",
-    letterSpacing: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
 });

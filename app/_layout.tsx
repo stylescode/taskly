7767b025e2fa3a3aa9ecce25240e1af6990c7657
Styles <stylesk11@gmail.com>
2024-08-21 13:24:32 -0700

    create tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,42 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colorCerulean,
+        tabBarInactiveTintColor: theme.colorGrey,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="list" size={size} color={color} solid />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="plus" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome6 name="lightbulb" size={size} color={color} solid />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }

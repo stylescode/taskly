034c955b2d31843cc82b18862dfaa100d1187314
Styles <stylesk11@gmail.com>
2024-08-21 13:42:21 -0700

    add link to history

--- app/counter/_layout.tsx
+++ app/counter/_layout.tsx
@@ -1,9 +1,30 @@
-import { Stack } from 'expo-router';
+import { Link, Stack } from "expo-router";
+import { Pressable, Text } from "react-native";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
+import { theme } from "../../theme";
 
 export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Counter",
+          headerRight: () => {
+            return (
+              <Link href={"/counter/history"} asChild>
+                <Pressable hitSlop={30}>
+                  <FontAwesome6
+                    name="clock-rotate-left"
+                    size={26}
+                    color={theme.colorDarkGrey}
+                  />
+                </Pressable>
+              </Link>
+            );
+          },
+        }}
+      />
     </Stack>
   );
 }

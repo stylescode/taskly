6393ec04b09533dbf2d18419f58ed842a2f44686
Styles <stylesk11@gmail.com>
2024-08-21 13:25:31 -0700

    move counter to new stack

---
+++ app/counter/index.tsx
@@ -0,0 +1,10 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function CounterScreen() {
+
+  return (
+    <View>
+      <Text>Counter</Text>
+    </View>
+  );
+}

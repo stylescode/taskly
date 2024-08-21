2b0bb658db3d1b9261150a1e64c6f47b4e52f302
Styles <stylesk11@gmail.com>
2024-08-21 14:54:59 -0700

    update colors

--- theme.ts
+++ theme.ts
@@ -2,7 +2,7 @@ export const theme = {
   colorCerulean: "#1a759f",
   colorWhite: "#fff",
   colorBlack: "#000",
-  colorGrey: "grey",
-  colorDarkGrey: "darkgrey",
+  colorGrey: "lightgrey",
+  colorDarkGrey: "grey",
   colorRed: "red",
 };

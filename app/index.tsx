e4aa1cd0dbba9ccde9e85c19b4e01e49e00876d2
Styles <stylesk11@gmail.com>
2024-08-21 14:54:49 -0700

    add text input

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-
+import { StyleSheet, TextInput, View } from "react-native";
+import { Link } from "expo-router";
 import { ListItem } from "../components/ListItem";
+import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  itemName: string;
+  isCompleted: boolean;
+};
+
+const initialItems: ShoppingListItemType[] = [
+  {
+    id: "1",
+    itemName: "TEA",
+    isCompleted: false,
+  },
+  {
+    id: "2",
+    itemName: "SUGAR",
+    isCompleted: true,
+  },
+  {
+    id: "3",
+    itemName: "CHOCOLATE",
+    isCompleted: false,
+  },
+];
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [shoppingList, setShoppingList] = useState(initialItems);
+
+  const handleAddItem = () => {
+    if (newItem.length > 0) {
+      const newShoppingList = [
+        ...shoppingList,
+        { id: Date.now().toString(), itemName: newItem, isCompleted: false },
+      ];
+      setShoppingList(newShoppingList);
+      setNewItem("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ListItem itemName="TEA" isCompleted={false} />
-      <ListItem itemName="SUGAR" isCompleted={true} />
-      <ListItem itemName="CHOCOLATE" isCompleted={false} />
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g. Coffee"
+        value={newItem}
+        onChangeText={setNewItem}
+        returnKeyType="done"
+        onSubmitEditing={handleAddItem}
+      />
+      {shoppingList.map((item) => (
+        <ListItem
+          key={item.id}
+          itemName={item.itemName}
+          isCompleted={item.isCompleted}
+        />
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +70,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+    paddingTop: 20,
+  },
+  textInput: {
+    borderColor: theme.colorGrey,
+    borderRadius: 10,
+    borderWidth: 1,
+    fontSize: 18,
+    marginBottom: 10,
+    marginHorizontal: 10,
+    padding: 10,
   },
 });

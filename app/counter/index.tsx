ece212f9fef50b1121d3c388aa0da98d47f4ecfe
Styles <stylesk11@gmail.com>
2024-08-27 12:57:23 -0700

    set up push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,56 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet, Alert } from "react-native";
+import { registerForPushNotifications } from "../../utils/registerForPushNotifications";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 export default function CounterScreen() {
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotifications();
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Taskly Reminder",
+          body: "Make sure your list up to date!",
+        },
+        trigger: {
+          hour: 9,
+          minute: 0,
+          repeats: true,
+        },
+      });
+    } else {
+      if (Device.isDevice)
+        Alert.alert(
+          "Notifications not permitted",
+          "Enable notifications in settings",
+        );
+    }
+  };
 
   return (
-    <View>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={scheduleNotification}>
+        <Text style={styles.buttonText}>Schedule Notification</Text>
+      </TouchableOpacity>
       <Text>Counter</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+  },
+});
---
+++ utils/registerForPushNotifications.ts
@@ -0,0 +1,28 @@
+import { Platform } from "react-native";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+
+export async function registerForPushNotifications() {
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.DEFAULT,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+      showBadge: false,
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      return status;
+    } else {
+      return existingStatus;
+    }
+  } else {
+    return null;
+  }
+}
